f51d902df62d049fadee2f624a4bffc480848436
Eskender Bilyalov <e.bilyalov@solarl.ru>
2019-08-23 11:32:40 +0300

    sa

---
+++ ToDoList/Controllers/TasksController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class TasksController : Controller
+    {
+        private TaskDbContext db = new TaskDbContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Tasks.ToList());
+        }
+
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Description,DateTime")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                task.DateTime = DateTime.Now;
+                db.Tasks.Add(task);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(task);
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Description,DateTime")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                task.DateTime = DateTime.Now;
+                db.Entry(task).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Task task = db.Tasks.Find(id);
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        /// <summary>
+        /// ИД задачи
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Описание задачи
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Дата создание задачи
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+    }
+
+    public class TaskDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
